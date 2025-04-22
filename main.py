b878c3aba48353b9d52b1ca86ad46440e6dbb20b
Victoria <adevictoria89@gmail.com>
2025-04-21 20:07:15 -0500

    bankingMenu

---
+++ main.py
@@ -0,0 +1,13 @@
+def menu():
+    print ("Welcome to Vic's Banking")
+    print("Select an option")
+    print("1. Create an Account")
+    print("2. Check Balance")
+    print("3. Deposit")
+    print("4. Withdraw")
+    print("5. Delete Account")
+    print("6. Modify Account")
+    print("0. Exit")
+
+    def create_Account():
+        name = input ("What is your first and last name?")
